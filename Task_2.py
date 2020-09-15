95a0a50e7b1a115eafbc36c19819817e1221fbff
Shikki <mary.sh95@gmail.com>
2020-09-15 23:10:19 +0300

    added final correction

--- Task_2.py
+++ Task_2.py
@@ -1,6 +1,5 @@
 #Задача 2. Напишите программу, которая принимает от пользователя число в диапазоне 1-99 и возвращает его прописью
-
-number = int(input('Введите число для перевода: '))
+string = input('Введите целое число или ряд целых чисел для перевода (от 1 до 99): ')
 
 a = {1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять',
      6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
@@ -10,21 +9,28 @@ b = {10: 'десять', 20: 'двадцать', 30: 'тридцать', 40: 'с
 c = {11: 'одиннадцать', 12: 'двенадцать', 13: 'тринадцать',
      14: 'четырнадцать', 15: 'пятнадцать', 16: 'шестнадцать',
      17: 'семнадцать', 18: 'восемнадцать', 19: 'девятнадцать'}
-number1 = number % 10
-number2 = number - number1
-for i in range(number):
-    if number < 10:
-        print(a.get(number).capitalize())
-        break
-    elif 10 < number < 20:
-        print(c.get(number).capitalize())
-        break
-    elif number >= 10 and number in b:
-        print(b.get(number).capitalize())
-        break
-    elif number >= 100:
-        print('Ваше число не находится в диапазоне 1-99')
-        break
-    else:
-        print((b.get(number2) + ' ' + a.get(number1)).capitalize())
-        break
+
+number_list = string.replace(',', '').split()
+
+for i in number_list:
+    i = [int(num) for num in filter(lambda num: num.isnumeric(), number_list)]
+    break
+for number in i:
+    number1 = number % 10
+    number2 = number - number1
+    for j in range(number):
+        if number < 10:
+            print(f'Вы ввели: {number}, это число прописью: {a.get(number).capitalize()}')
+            break
+        elif 10 < number < 20:
+            print(f'Вы ввели: {number}, это число прописью: {c.get(number).capitalize()}')
+            break
+        elif number >= 10 and number in b:
+            print(f'Вы ввели: {number}, это число прописью: {b.get(number).capitalize()}')
+            break
+        elif number >= 100:
+            print(f'Число {number} не находится в диапазоне 1-99')
+            break
+        else:
+            print(f"Вы ввели: {number}, ваше число прописью: {(b.get(number2) + ' ' + a.get(number1)).capitalize()}")
+            break
